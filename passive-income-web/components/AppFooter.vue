9bf33cf0024f78a33c1c78b7bc5c3a544b8bd773
penpengusa2k <tinnanagokunn@gmail.com>
2025-06-29 15:33:10 +0900

    GitHubのURL更新、プライバシーポリシー修正

--- passive-income-web/components/AppFooter.vue
+++ passive-income-web/components/AppFooter.vue
@@ -6,14 +6,11 @@
       </p>
 
       <div class="mt-4 space-x-4">
-        <a href="https://twitter.com/your_twitter_handle" target="_blank" rel="noopener noreferrer" class="text-gray-500 hover:text-blue-500 transition-colors duration-200">
-          Twitter
-        </a>
-        <a href="https://github.com/your_github_profile" target="_blank" rel="noopener noreferrer" class="text-gray-500 hover:text-gray-700 transition-colors duration-200">
+        <a href="https://github.com/penpengusa2k/" target="_blank" rel="noopener noreferrer" class="text-gray-500 hover:text-gray-700 transition-colors duration-200">
           GitHub
         </a>
         <NuxtLink to="/privacy-policy" class="text-gray-500 hover:text-gray-700 transition-colors duration-200">
-          プライバシーポリシー
+          privacy-policy
         </NuxtLink>
       </div>
 
